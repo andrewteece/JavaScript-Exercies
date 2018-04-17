279289799d5e8fa72a79bdd066e37ac87117c155
andrewteece <andrewteece@gmail.com>
2018-04-17 17:38:00 -0500

    ice cream

---
+++ moreJS.js
@@ -0,0 +1,11 @@
+// JS Quiz: Ice-Cream 3-6
+
+let flavor = 'strawberry';
+let vessel = 'cone';
+let toppings = 'cookies';
+
+if (((flavor === 'vanilla') || (flavor === 'chocolate'))
+&& ((vessel === 'cone') || vessel === 'bowl'))
+&& ((toppings === 'sprinkles') || (toppings === 'peanuts')) {
+    console.log('I would like two scoops of ' flavor + ' ice cream in a ' + vessel + ' with toppings + '.'';
+}
